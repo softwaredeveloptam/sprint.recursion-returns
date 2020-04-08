52d267894cc75e3e36b2cc2af930a010bc958a1a
maegatro <shuntaromaekawa@gmail.com>
2020-04-08 21:59:53 +0900

    Finished getElementsByClassName.js
    Co-authored-by: softwaredeveloptam <softwaredeveloptam@users.noreply.github.com>

--- spec/_getElementsByClassName.js
+++ spec/_getElementsByClassName.js
@@ -55,34 +55,8 @@ describe.only("getElementsByClassName", () => {
         "targetClassName"
       );
       const expectedArray = Array.prototype.slice.apply(expectedNodeList);
-      console.log(expectedArray);
-      // expect(result).to.eql(expectedArray);
+      expect(result).to.eql(expectedArray);
       $rootElement.remove();
     });
   }
 });
-
-// const htmlStrings = [
-//   "<p></p>",
-//   '<div><div class="testing"></div></div>',
-//   '<div><div class="targetClassName"></div></div>',
-//   '<div><button class="targetClassName"></button></div><div class="classname"></div>',
-//   '<div><p class="targetClassName"><b>asdfasdf</b></p></div><div class="classname"><span></span></div>',
-//   '<div><p class="targetClassName"><b>asdfasdf</b></p></div><div class="classname"><span class="span test"></span></div>',
-//   '<div class="a"><p class="targetClassName asdf"><b>aasdfasdfsdfasdf</b></p></div><div class="classname"><span class="span test"></span></div>',
-// ];
-
-// for (let i = 0; i < htmlStrings.length; i++) {
-//   testHTMLStrings(htmlStrings[i], i);
-// }
-
-// function testHTMLStrings(htmlString, index) {
-//     const $rootElement = $(htmlString);
-//     $("body").append($rootElement);
-//     const expectedNodeList = document.getElementsByClassName(
-//       "targetClassName"
-//     );
-//     const expectedArray = Array.prototype.slice.apply(expectedNodeList);
-//     console.log(expectedArray)
-//   };
-// }

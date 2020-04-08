e025b8292d29603d3e4a15542569182ee9322f6e
maegatro <shuntaromaekawa@gmail.com>
2020-04-09 08:17:53 +0900

    Changed function

--- src/robotPaths.js
+++ src/robotPaths.js
@@ -1,6 +1,6 @@
 class Board {
   constructor(size) {
-    this.board = []; //[[false], [false], [false]]  size = 3
+    this.board = []; //[[false, false, false], [false, false, false], [false, false, false]]  size = 3  row[0, 1 , 2] col[0, 1, 2]
     for (let row = 0; row < size; row += 1) {
       this.board.push([]);
       for (let col = 0; col < size; col += 1) {
@@ -28,24 +28,28 @@ class RobotPaths {
     this.size = size;
   }
 
-  solve() {
-    let result;
-
-    if (this.size == 1) {
-      return (result = 1);
-    } else if (this.size == 2) {
-      return (result = 2);
-    } else if (this.size == 3) {
-      return (result = 12);
-    } else if (this.size == 4) {
-      return (result = 184);
-    } else if (this.size == 5) {
-      return (result = 8512);
-    } else if (this.size == 6) {
-      return (result = 1262816);
+  solve(x, y) {
+    let paths = 0;
+    if (x === this.size - 1 && y === this.size - 1) {
+      //exit case
+      paths++;
+      return;
     }
-
-    return result;
+    if (x < 0 || x >= this.size || y < 0 || y >= this.size) {
+      return;
+    }
+    if (this.board.hasBeenVisited(x, y)) {
+      return;
+    } else {
+      this.board.togglePiece(x, y);
+      this.solve(x, y + 1);
+      this.solve(x + 1, y);
+      this.solve(x, y - 1);
+      this.solve(x - 1, y);
+      this.board.togglePiece(x, y);
+    }
+    this.solve(0, 0);
+    return paths;
   }
 }
 

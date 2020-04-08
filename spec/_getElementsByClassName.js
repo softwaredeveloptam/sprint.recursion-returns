f55593935b171dd6b6132c2c62af1d39eface4c0
maegatro <shuntaromaekawa@gmail.com>
2020-04-08 19:48:39 +0900

    first commit
    Co-authored-by: softwaredeveloptam <softwaredeveloptam@users.noreply.github.com>

--- spec/_getElementsByClassName.js
+++ spec/_getElementsByClassName.js
@@ -19,7 +19,7 @@ const htmlStrings = [
   '<div class="a"><p class="targetClassName asdf"><b>aasdfasdfsdfasdf</b></p></div><div class="classname"><span class="span test"></span></div>',
 ];
 
-describe("getElementsByClassName", () => {
+describe.only("getElementsByClassName", () => {
   beforeEach(() => {
     $("body").addClass("targetClassName");
   });
@@ -55,8 +55,34 @@ describe("getElementsByClassName", () => {
         "targetClassName"
       );
       const expectedArray = Array.prototype.slice.apply(expectedNodeList);
-      expect(result).to.eql(expectedArray);
+      console.log(expectedArray);
+      // expect(result).to.eql(expectedArray);
       $rootElement.remove();
     });
   }
 });
+
+// const htmlStrings = [
+//   "<p></p>",
+//   '<div><div class="testing"></div></div>',
+//   '<div><div class="targetClassName"></div></div>',
+//   '<div><button class="targetClassName"></button></div><div class="classname"></div>',
+//   '<div><p class="targetClassName"><b>asdfasdf</b></p></div><div class="classname"><span></span></div>',
+//   '<div><p class="targetClassName"><b>asdfasdf</b></p></div><div class="classname"><span class="span test"></span></div>',
+//   '<div class="a"><p class="targetClassName asdf"><b>aasdfasdfsdfasdf</b></p></div><div class="classname"><span class="span test"></span></div>',
+// ];
+
+// for (let i = 0; i < htmlStrings.length; i++) {
+//   testHTMLStrings(htmlStrings[i], i);
+// }
+
+// function testHTMLStrings(htmlString, index) {
+//     const $rootElement = $(htmlString);
+//     $("body").append($rootElement);
+//     const expectedNodeList = document.getElementsByClassName(
+//       "targetClassName"
+//     );
+//     const expectedArray = Array.prototype.slice.apply(expectedNodeList);
+//     console.log(expectedArray)
+//   };
+// }
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,65 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (target) => {
+  let result = [];
+  let length = target.length;
+
+  function recursive(className) {
+    if (target[length] == className) {
+      // exit
+      result.push(className);
+    } else if (length == 0) {
+    } else {
+      recursive(className, length - 1); // recursion
+    }
+  }
+
+  return result;
 };
+
+/*
+let obj = {};
+
+[
+  HTMLBodyElement {},
+  HTMLDivElement {},
+  HTMLButtonElement {},
+  HTMLParagraphElement {},
+  HTMLParagraphElement {},
+  HTMLParagraphElement {}
+]
+
+'<div class="a">
+    <p class="targetClassName asdf"><b>aasdfasdfsdfasdf</b></p>
+ </div>
+ <div class="classname">
+  <span class="span test"></span>
+</div>'
+
+//How does the dom should look like??
+{
+  {
+    {
+
+    }
+  },
+  {
+    {
+
+    }
+  },
+}
+*/
+
+/*
+
+const htmlStrings = [
+  "<p></p>",
+  '<div><div class="testing"></div></div>',
+  '<div><div class="targetClassName"></div></div>',
+  '<div><button class="targetClassName"></button></div><div class="classname"></div>',
+  '<div><p class="targetClassName"><b>asdfasdf</b></p></div><div class="classname"><span></span></div>',
+  '<div><p class="targetClassName"><b>asdfasdf</b></p></div><div class="classname"><span class="span test"></span></div>',
+  '<div class="a"><p class="targetClassName asdf"><b>aasdfasdfsdfasdf</b></p></div><div class="classname"><span class="span test"></span></div>',
+]
+*/
 
 module.exports = { getElementsByClassName };
